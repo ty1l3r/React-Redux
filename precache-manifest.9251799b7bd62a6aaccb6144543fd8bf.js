ebe38d6393c57be28606612c9a05d126b4fe0c6b
ty1l3r <ak-vitrail@live.fr>
2020-09-14 12:34:58 +0200

    Updates

---
+++ precache-manifest.9251799b7bd62a6aaccb6144543fd8bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3112ed85e37555d1e5eb21e51b8eb6e5",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "af44a2d41a708319df87",
+    "url": "/React-Redux/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "4e3c8f4d540074b2079d",
+    "url": "/React-Redux/static/js/2.1ffdbab8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/React-Redux/static/js/2.1ffdbab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af44a2d41a708319df87",
+    "url": "/React-Redux/static/js/main.5d3a17a7.chunk.js"
+  },
+  {
+    "revision": "4a76077ecddb5fbd58e4",
+    "url": "/React-Redux/static/js/runtime-main.512ace37.js"
+  }
+]);
